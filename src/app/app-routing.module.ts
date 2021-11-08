9fef0e4f672511e6cb895b4793792c39398cb35a
Christopher Pearson <cjpearson@hotmail.com>
2021-11-08 22:58:52 +0000

    update app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookComponent } from './book/book.component';
+import { BookshelfComponent } from './bookshelf/bookshelf.component';
+import { ChapterComponent } from './chapter/chapter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
+  { path: 'books', component: BookshelfComponent },
+  { path: 'books/:bookId', component: BookComponent },
+  { path: 'books/:bookId/chapters/:chapterId', component: ChapterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookshelfComponent } from './bookshelf/bookshelf.component';
+import { BookComponent } from './book/book.component';
+import { ChapterComponent } from './chapter/chapter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookshelfComponent,
+    BookComponent,
+    ChapterComponent
   ],
   imports: [
     BrowserModule,
