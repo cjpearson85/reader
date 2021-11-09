17422959db27922a59d72c6585f45f7dbba7818a
Christopher Pearson <cjpearson@hotmail.com>
2021-11-09 12:51:14 +0000

    update chapter component

--- src/app/chapter/chapter.component.ts
+++ src/app/chapter/chapter.component.ts
@@ -1,27 +1,47 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Book, Chapter } from '../book';
+
+import { Book } from '../book';
+import { BooksService } from '../books.service';
 
 @Component({
   selector: 'app-chapter',
   templateUrl: './chapter.component.html',
   styleUrls: ['./chapter.component.css'],
+  providers: [BooksService],
 })
 export class ChapterComponent {
-  book: Book;
-  chapter: Chapter;
-  bookId;
-  currentChapterIndex;
-  nextChapterIndex;
+  book: Book | undefined;
+  bookId: number = 0;
+  chapterId: number = 0;
+  nextChapterId: number = 0;
 
-  constructor(private route: ActivatedRoute) {
-    this.book = history.state.data.book;
-    this.bookId = history.state.data.bookId;
-    this.currentChapterIndex = parseInt(
+  constructor(
+    private booksService: BooksService,
+    private route: ActivatedRoute
+  ) {
+    this.route.paramMap.subscribe((params) => {
+      this.ngOnInit();
+    });
+  }
+
+  ngOnInit(): void {
+    this.getBook();
+  }
+
+  getBook(): void {
+    const bookId = parseInt(this.route.snapshot.paramMap.get('bookId')!, 10);
+    this.bookId = bookId;
+    this.book = this.booksService.getBook(bookId);
+    const chapterId = parseInt(
       this.route.snapshot.paramMap.get('chapterId')!,
       10
     );
-    this.chapter = this.book.chapters[this.currentChapterIndex];
-    this.nextChapterIndex = this.currentChapterIndex + 1;
+    this.chapterId = chapterId;
+    if (chapterId >= this.book.chapters.length - 1) {
+      this.nextChapterId = 0;
+    } else {
+      this.nextChapterId = chapterId + 1;
+    }
   }
 }
