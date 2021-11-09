7a75d847f0ad160589980beba8ed003f1fa06e1e
Christopher Pearson <cjpearson@hotmail.com>
2021-11-09 12:50:58 +0000

    update book component

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { Book } from '../book';
+import { BooksService } from '../books.service';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css'],
+  providers: [BooksService],
 })
 export class BookComponent {
   book: Book;
-  bookId = 0;
+  bookId: number;
 
-  constructor() {
-    this.book = history.state.data.book;
-    this.bookId = history.state.data.bookId;
+  constructor(
+    private booksService: BooksService,
+    private route: ActivatedRoute
+  ) {
+    this.bookId = parseInt(this.route.snapshot.paramMap.get('bookId')!, 10);
+    this.book = this.booksService.getBook(this.bookId);
   }
 }
